7e64646397ad3c143a10f1fa841b5300e9fc9299
Tom Mackall <mackall.tom@gmail.com>
2016-01-01 07:57:50 -0700

    lesson 6

--- snippets/views.py
+++ snippets/views.py
@@ -1,16 +1,14 @@
 from django.contrib.auth.models import User
 
-from rest_framework import generics
 from rest_framework import permissions
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, detail_route
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
-from rest_framework import renderers
+from rest_framework import renderers, viewsets
 
 
-from snippets.serializers import UserSerializer
+from snippets.serializers import UserSerializer, SnippetSerializer
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
 from snippets.permissions import IsOwnerOrReadOnly
 
 
@@ -23,36 +21,31 @@ def api_root(request, format=None):
     })
 
 
-class SnippetHighlight(generics.GenericAPIView):
-    queryset = Snippet.objects.all()
-    renderer_classes = (renderers.StaticHTMLRenderer,)
-
-    def get(self, request, *args, **kwargs):
-        snippet = self.get_object()
-        return Response(snippet.highlighted)
-
-
-class UserList(generics.ListAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `detail` actions.
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
-class UserDetail(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+class SnippetViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
 
-
-class SnippetList(generics.ListCreateAPIView):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
-
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    Additionally we also provide an extra `highlight` action.
+    """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly,)
+
+    @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
--- test.sh
+++ test.sh
@@ -2,4 +2,27 @@
 
 cmd="http -a admin:rootpass http://192.168.1.18:8051/snippets/"
 echo "$cmd"
+resp=$(eval $cmd)
+echo "$resp"
+exit 0
+
+cmd="http -a admin:rootpass PUT http://192.168.1.18:8051/snippets/ code=\"print tmac\""
+echo "$cmd"
+resp=$(eval $cmd)
+if [[ ! ${resp} =~ .*not\ allowed.* ]]; then
+    echo "$resp"
+    exit 2
+fi
+
+cmd="http -a admin:rootpass POST http://192.168.1.18:8051/snippets/ code=\"print tmac\""
+echo "$cmd"
+eval $cmd
+exit 0
+
+cmd="http -a admin:rootpass http://192.168.1.18:8051/snippets/"
+echo "$cmd"
+eval $cmd
+
+cmd="http -a admin:rootpass http://192.168.1.18:8051/snippets/"
+echo "$cmd"
 eval $cmd
